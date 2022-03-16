5ac2bfcbdf4a8d5306e08a359d46f8bcabc66b4c
salavat <ellight22@gmail.com>
2022-03-16 15:56:07 +0300

    second commit

---
+++ src/main/java/com/bic/spring/security/configuration/MySecurityInintzialer.java
@@ -0,0 +1,6 @@
+package com.bic.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInintzialer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/bic/spring/security/configuration/MyWebInitzialer.java
@@ -0,0 +1,20 @@
+package com.bic.spring.security.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitzialer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
